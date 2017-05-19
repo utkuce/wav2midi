1260cb57726da659d11349dce2297115afd026ce
utku <utkuce@gmail.com>
2017-05-19 15:28:50 +0300

    reads wav file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-fn main() {
-    println!("Hello, world!");
-    test();
-}
+extern crate hound;
 
-fn test() {
-    println!("asd");
+fn main() {
+
+    let fourier_window = 512;
+
+    let mut reader = hound::WavReader::open("C:/Users/utku/Desktop/3_2.wav").unwrap();    
+    let audio_data = reader.samples::<i16>().map( |sample| sample.unwrap() as f64);
+
+    let mut fft_in : Vec<f64> = Vec::new();
+    for (index, sample) in audio_data.enumerate() {
+        fft_in.push(sample);
+        if index % fourier_window == 0 {
+            fft_in.clear();
+            println!("{} {}", index, sample);
+        }
+    }
 }
