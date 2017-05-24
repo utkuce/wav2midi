86ccb9e41cfacde546ca145dd17e4166dfdd35c7
utku <utkuce@gmail.com>
2017-05-24 19:48:30 +0300

    variance

--- spectrogram.py
+++ spectrogram.py
@@ -12,17 +12,19 @@ def get_heatmap(filename):
     heatmap = np.array(object=heatmap, dtype=float)
     return heatmap
 
+print('reading values...')
+
 heatmap1 = get_heatmap('spectrogram1.csv')
 heatmap2 = get_heatmap('spectrogram2.csv')
 
-averages = []
+print('calculating...')
+
+variances = []
 
 for fourier in heatmap1:
-    total = 0
-    for frequency, amplitude in enumerate(fourier):
-        total += frequency*amplitude
-    averages.append(total/len(fourier))
+    variances.append(np.var(fourier))
 
+#transpose
 heatmap1 = [list(x) for x in zip(*heatmap1)]
 heatmap2 = [list(x) for x in zip(*heatmap2)]
 
@@ -31,6 +33,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(2,1,1)
 ax2 = ax1.twinx()
 
+ax1.set_title('Window Size: {} Step size: {}'.format(4096, 512))
 ax1.set_xlabel('Number of Fourier Transforms')
 ax1.set_ylabel('Frequency(Hz)')
 ax1.imshow(heatmap1, cmap='inferno', interpolation='nearest', aspect='auto')
@@ -38,14 +41,17 @@ ax1.invert_yaxis()
 
 plt.xlim(xmin=0)
 
-ax2.set_ylabel('Weighed Average Frequency')
-ax2.plot(averages, 'w-')
+ax2.set_ylabel('Frequency Variance')
+ax2.plot(variances, 'w-')
 
 ax3 = fig.add_subplot(2,1,2)
+ax3.set_title('Window Size: {} Step size: {}'.format(44100, 512))
 ax3.set_xlabel('Number of Fourier Transforms')
 ax3.set_ylabel('Frequency(Hz)')
 ax3.imshow(heatmap2, cmap='inferno', interpolation='nearest', aspect='auto')
 ax3.invert_yaxis()
+
+#plt.colorbar(ax1.pcolor(heatmap1, cmap='inferno'))
 
 fig.tight_layout()
 
