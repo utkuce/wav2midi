a3a843f27c69d098ccef7610856552e6bbb9289e
utku <utkuce@gmail.com>
2017-10-24 19:28:18 +0300

    frequency as integer

--- src/postprocess.rs
+++ src/postprocess.rs
@@ -1,7 +1,6 @@
 extern crate rimd;
 
-use std::os::raw;
-use std::{f64, fmt, slice};
+use std::{f64, fmt};
 use self::rimd::{ TrackEvent, Event, MidiMessage, SMFWriter, SMFFormat, Track, SMF};
 use std::path::Path;
 
@@ -17,15 +16,15 @@ impl fmt::Display for Song {
     }
 }
 
-fn get_pitch<'a>(frequency : &'a f64) -> (String, u8) 
+fn get_pitch(frequency : u32) -> (String, u8) 
 {
-    if frequency.round() as i32 == 0 { return (String::from("R"), 0); } // TODO: midi rest
+    if frequency == (0 as u32) { return (String::from("R"), 0); } // TODO: midi rest
 
     let notes = vec![ "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" ];
     
     //distance to A440 of the note
     let log_base : f64 = (2.0 as f64).powf(1.0/12.0);   
-    let mut offset : i32 = ((frequency / 440.0).log(log_base)).round() as i32;
+    let mut offset : i32 = ((frequency as f64 / 440.0).log(log_base)).round() as i32;
 
     let midi = 69 + offset; // A4 in midi is 69
     
@@ -35,11 +34,9 @@ fn get_pitch<'a>(frequency : &'a f64) -> (String, u8)
     return (format!( "{note}{octave}", note = notes[note_index], octave = 4 + offset/12), midi as u8);
 }
 
-pub fn get_notes(frequencies: *const raw::c_double, len: raw::c_uint) -> Vec<Note>
+pub fn get_notes(frequencies: Vec<u32>) -> Vec<Note>
 {
-    let data: &[f64] = unsafe { slice::from_raw_parts(frequencies, len as usize) };
-    let vec : Vec<(String, u8)> = data.to_vec().iter().map(|freq| get_pitch(freq)).collect();
-
+    let vec : Vec<(String, u8)> = frequencies.iter().map(|freq| get_pitch(*freq)).collect();
     let mut notes : Vec<Note> = Vec::new();
 
     let mut last: usize = 0;
